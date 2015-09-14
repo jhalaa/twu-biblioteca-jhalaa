7f0829fc071a76a5bafe2136fa8140daf3b8bc4f
jhalaa <jhalaachinoy@gmail.com>
2015-09-14 22:44:08 +0530

    movie should have a valid year

--- src/com/thoughtworks/biblioteca/Movies.java
+++ src/com/thoughtworks/biblioteca/Movies.java
@@ -2,8 +2,13 @@ package com.thoughtworks.biblioteca;
 
 public class Movies {
     private String movieName = "Titanic";
+    private int movieYear = 1990;
 
     public String getMovieName() {
         return movieName;
+    }
+
+    public int getMovieYear() {
+        return movieYear;
     }
 }
--- test/com/thoughtworks/biblioteca/MoviesTest.java
+++ test/com/thoughtworks/biblioteca/MoviesTest.java
@@ -7,11 +7,15 @@ import static org.junit.Assert.assertEquals;
 public class MoviesTest {
 
     @Test
-    public void shouldHaveAName()
-    {
+    public void shouldHaveAName() {
         Movies movies = new Movies();
         assertEquals("Titanic",movies.getMovieName());
     }
 
+    @Test
+    public void shouldHaveAYear() {
+        Movies movies = new Movies();
+        assertEquals(1990,movies.getMovieYear());
+    }
 
 }
