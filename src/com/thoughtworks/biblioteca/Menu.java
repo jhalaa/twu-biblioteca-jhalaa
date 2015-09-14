f5da41e79ad5ac5d9174af02299b8c1141bf4e7a
jhalaa <jhalaachinoy@gmail.com>
2015-09-14 22:56:07 +0530

    modified menu to have movie checkout

--- src/com/thoughtworks/biblioteca/Menu.java
+++ src/com/thoughtworks/biblioteca/Menu.java
@@ -3,7 +3,7 @@ package com.thoughtworks.biblioteca;
 //returns the menu
 public class Menu {
 
-    private static String menuList = "1:List of books\t2:Exit\t3:Checkout\t4:Return";
+    private static String menuList = "1:List of books\t2:Exit\t3:Checkout Books\t4:Return\tCheckout Movies";
     private Printer printer;
 
     public Menu(Printer printer) {
