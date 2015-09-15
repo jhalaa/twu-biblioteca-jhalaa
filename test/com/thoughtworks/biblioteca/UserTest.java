3ef87cb0b78c32680171488ec8debe3f61d04ba6
jhalaa <jhalaachinoy@gmail.com>
2015-09-15 17:11:09 +0530

    modified the user class to invalidate login if they have different no and password

--- test/com/thoughtworks/biblioteca/UserTest.java
+++ test/com/thoughtworks/biblioteca/UserTest.java
@@ -3,8 +3,30 @@ package com.thoughtworks.biblioteca;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class UserTest {
+
+    @Test
+    public void shouldBeDifferentIfTheyHaveSameUserNameAndDifferentPassword() {
+        User user1 = new User("123-1234","xhy");
+        User user2 = new User("123-1234","xyz");
+        assertNotEquals(user1, user2);
+    }
+
+    @Test
+    public void shouldBeDifferentIfTheyHaveDifferentUserNameAndSamePassword() {
+        User user1 = new User("123-1334","xyz");
+        User user2 = new User("123-1234","xyz");
+        assertNotEquals(user1, user2);
+    }
+
+    @Test
+    public void shouldBeDifferentIfTheyHaveDifferentUserNameAndDifferentPassword() {
+        User user1 = new User("123-1334","xhz");
+        User user2 = new User("123-1234","xyz");
+        assertNotEquals(user1, user2);
+    }
 
     @Test
     public void shouldBeSameIfTheyHaveSameUserNameAndPassword() {
