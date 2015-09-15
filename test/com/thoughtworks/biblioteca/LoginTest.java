7510965903648868cfecf5d0a42d837322685ee6
jhalaa <jhalaachinoy@gmail.com>
2015-09-15 16:51:01 +0530

    must invalidate an invalid user

--- test/com/thoughtworks/biblioteca/LoginTest.java
+++ test/com/thoughtworks/biblioteca/LoginTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class LoginTest {
@@ -17,7 +18,7 @@ public class LoginTest {
         users.add(new User("122-4517","xyz"));
         users.add(new User("123-2227","jksx"));
         Login login = new Login(users);
-        assertEquals(users,login.users);
+        assertEquals(users, login.users);
     }
 
     @Test
@@ -31,4 +32,18 @@ public class LoginTest {
         User user1 = new User("123-4567", "jhalaa");
         assertTrue(login.validateUser(user1));
     }
+
+    @Test
+    public void shouldInvalidateAnInvalidUser() {
+        ArrayList<User> users = new ArrayList<User>();
+        users.add(new User("123-4567","jhalaa"));
+        users.add(new User("123-4577","miffy"));
+        users.add(new User("122-4517","xyz"));
+        users.add(new User("123-2227","jksx"));
+        Login login = new Login(users);
+        User user1 = new User("123-4568", "jhalaa");
+        assertFalse(login.validateUser(user1));
+    }
+
+
 }
